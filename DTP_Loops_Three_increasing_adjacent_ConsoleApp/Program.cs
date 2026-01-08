54d0d17c9386f76e210ef8ebba496951da335a53
Tobias R. <no@thankyou.com>
2026-01-08 12:00:25 +0100

    Added "Three increased adjacement" project solution

---
+++ DTP_Loops_Three_increasing_adjacent_ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DTP_Loops_Three_increasing_adjacent_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array = { 7, 3, 5, 8, 9, 3, 1, 4 };
+            Console.WriteLine(ThreeIncreasingAdjacent(array));
+        }
+        static bool ThreeIncreasingAdjacent(float[] array)
+        {
+            for (int i = 0; i < array.Length-2; i++)
+            {
+                if (array[i + 1] == array[i] + 1 && array[i + 2] == array[i + 1] + 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
