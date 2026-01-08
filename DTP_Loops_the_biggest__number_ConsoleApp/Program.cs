94648bdf96554b37ef0465232f200b5fe23f1026
Tobias R. <no@thankyou.com>
2026-01-08 08:45:37 +0100

    Added "The biggest number" project solution

---
+++ DTP_Loops_the_biggest__number_ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DTP_Loops_the_biggest__number_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =
+            {
+                1,
+                2,
+                5,
+                2,
+                15,
+                34,
+                3,
+                -20,
+                0
+            };
+            Console.WriteLine(GetBiggestNumberInArray(array));
+        }
+        static int GetBiggestNumberInArray(int[] array)
+        {
+            int largestNum = 0;
+            foreach (int item in array)
+            {
+                if (item > largestNum)
+                {
+                    largestNum = item;
+                }
+            }
+            return largestNum;
+        }
+    }
+}
---
+++ DTP_Loops_the_biggest_number_ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DTP_Loops_the_biggest_number_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =
+            {
+                1,
+                2,
+                5,
+                2,
+                15,
+                34,
+                3,
+                -20,
+                0
+            };
+
+        }
+        static int GetBiggestNumberInArray(int[] array)
+        {
+            int largestNum = 0;
+            foreach (int item in array)
+            {
+                if (item > largestNum)
+                {
+                    largestNum = item;
+                }
+            }
+            return largestNum;
+        }
+    }
+}
