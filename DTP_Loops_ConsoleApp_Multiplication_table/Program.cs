d9911fa20e02e4985ba2a3998b1ac3fc1de060a4
Tobias R. <no@thankyou.com>
2026-01-08 08:17:11 +0100

    Add project files.

---
+++ DTP_Loops_ConsoleApp_Multiplication_table/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DTP_Loops_ConsoleApp_Multiplication_table
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                table(i);
+                Console.Write("\n");
+            }
+        }
+        static void table(int mult)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i * mult} ");
+            }
+        }
+    }
+}
