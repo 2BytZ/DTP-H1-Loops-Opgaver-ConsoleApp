55c7e0ab44572444f331cf5bbd821c52e72e7130
Tobias R. <no@thankyou.com>
2026-01-08 09:35:50 +0100

    Added "Two 7s next to each other" project solution

---
+++ DTP_Loops_two_7s_beside_each_other_ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DTP_Loops_two_7s_beside_each_other_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array =
+            {
+                1,2,5,6,7,7,7,8,7,7,7,7,10,20,303
+            };
+
+            Console.WriteLine(Two7sNextToEachOther(array));
+        }
+        static int Two7sNextToEachOther(float[] array)
+        {
+            int previousNum = 0;
+            int i = 0; // 7 count
+            foreach (int item in array)
+            {
+                int currentNum = item;
+                if (currentNum == 7 && previousNum == 7)
+                {
+                    i++;
+                }
+                previousNum = currentNum;
+            }
+            return i;
+        }
+    }
+}
