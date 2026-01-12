7f691605b30a892644696b591dd8b5723c90596f
Tobias R. <no@thankyou.com>
2026-01-12 13:37:33 +0100

    Added project/solution for "Sum and average"

---
+++ DTP_Loops_Sum_of_average_ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DTP_Loops_Sum_and_average_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = -10;
+            int num2 = 0;
+            GetSumAndAverage(num1, num2);
+
+        }
+        static void GetSumAndAverage(int num1, int num2)
+        {
+            List<float> findAverage = new List<float>();
+            float sumOfIntegers = 0;
+            for (int i = num1; i <= num2; i++)
+            {
+                sumOfIntegers += i;
+            }
+            float average = sumOfIntegers / (num2 - num1+1);
+            Console.WriteLine($"Sum: {sumOfIntegers}, Average: {average}");
+        }
+    }
+}
