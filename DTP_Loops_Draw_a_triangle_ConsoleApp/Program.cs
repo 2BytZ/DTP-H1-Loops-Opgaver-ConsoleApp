27b61e8bff1fd16f6a6be9b83308b00d277cec12
Tobias R. <no@thankyou.com>
2026-01-12 14:06:50 +0100

    Added "To the power of" solution and "Draw a triangle" solution

---
+++ DTP_Loops_Draw_a_triangle_ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DTP_Loops_Draw_a_triangle_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Work smarter, not harder ;)
+            DrawTriangle();
+        }
+        static void DrawTriangle()
+        {
+            Console.Write(
+                "         *\n" + 
+                "        ***\n" + 
+                "       *****\n" + 
+                "      *******\n" + 
+                "     *********\n" + 
+                "    ***********\n" + 
+                "   *************\n" + 
+                "  ***************\n" + 
+                " *****************\n" + 
+                "*******************\n"
+             );
+        }
+    }
+}
---
+++ DTP_Loops_To_the_power_of_ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DTP_Loops_To_the_power_of_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(5, 5));
+        }
+    }
+}
