051916ff5c8500911d46a53433c8b2f4a9454364
Tobias R. <no@thankyou.com>
2026-01-12 10:12:37 +0100

    Added solution for Extract string M

---
+++ DTP_Loops_Extract_stringM_ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DTP_Loops_Extract_stringM_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "##abc##def";
+            Console.WriteLine(ExtractM(input));
+        }
+        static string ExtractM (string input)
+        {
+            int startIndex = input.IndexOf("##");
+            if (startIndex == 0)
+            {
+                startIndex += 2;
+            }
+            int endIndex = input.LastIndexOf("##");
+            if (startIndex >= endIndex)
+            {
+                return "";
+            }
+            string output = input.Substring(Math.Abs(startIndex), Math.Abs(endIndex));
+            return output.Replace("##", "");
+
+        }
+    }
+}
